fc9add4480b039b37db16920df5e48f89cb68bb5
mhshuvro <mainul.hasan.shuvro@gmail.com>
2021-01-25 00:33:08 +0100

    Added new pages with routing, font awesome, authentication

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <meta name="description" content="@yield('page_description', 'Social networking platform to share your hobbies')">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('page_title', 'My Hobbies')</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -33,7 +35,16 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li>
+                            <a class="nav-link{{ Request::is('/') ? ' active' : '' }}" href="/">
+                                Start
+                            </a>
+                        </li>
+                        <li>
+                            <a class="nav-link{{ Request::is('info') ? ' active' : '' }}" href="/info">
+                                Info
+                            </a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
---
+++ resources/views/starting_page.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Starting Page') }}</div>
+
+                <div class="card-body">
+                    {{ __('This is the starting page!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
